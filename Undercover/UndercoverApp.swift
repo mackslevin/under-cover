73ef69bf9da4035e4e7f9469575375d03c05fad9
Mack Slevin <mack.slevin@gmail.com>
2024-03-21 15:15:58 -0700

    Initial Commit

---
+++ Undercover/UndercoverApp.swift
@@ -0,0 +1,32 @@
+//
+//  UndercoverApp.swift
+//  Undercover
+//
+//  Created by Mack Slevin on 3/21/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct UndercoverApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
