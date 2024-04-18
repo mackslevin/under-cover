593c0d47e6358f24b69da310d4035aa58f78d7ba
Mack Slevin <mack.slevin@gmail.com>
2024-04-18 16:03:45 -0700

    Add UI for new favorites index list row. Logic/real data still needed.

---
+++ Undercover/Views/FavoritesListRow.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListRow.swift
+//  Undercover
+//
+//  Created by Mack Slevin on 4/18/24.
+//
+
+import SwiftUI
+
+struct FavoritesListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesListRow()
+}
